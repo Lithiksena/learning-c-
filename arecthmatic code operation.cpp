3632d07a1d7524c2f7b38d83fd27e07fdaee8019
Lithiksena <71975651+Lithiksena@users.noreply.github.com>
2020-09-27 16:21:12 +0200

    basic arithmetic code

---
+++ arecthmatic code operation.cpp
@@ -0,0 +1,26 @@
+	#include<iostream>
+	using namespace std;
+int main ()
+{
+	int a;
+	int b;
+	cin>>a>>b;
+	
+	cout<<"addition of a and b"<<endl;
+	int c= a+b;
+	cout<<c<<endl;
+	cout<<"subraction of a and b"<<endl;
+	int d = a-b;
+	cout<<d<<endl;
+	cout<<"multipliacation of a and b "<<endl;
+	int e = a*b;
+	cout<<e<<endl;
+	cout<<"division of a and b "<<endl;
+	int f = a/b;
+	cout<<f<<endl;
+	cout<<"modulus of a and b"<<endl;
+	int g = a%b;
+	cout<<g<<endl;
+}
+// while declaring of function prototype int is used whenever we need to return a value 
+// whenever we use void function does not return anythings 
