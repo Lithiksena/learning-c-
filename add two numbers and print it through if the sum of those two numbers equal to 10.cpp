f46dcdb1cee3667d883f1aafe699a43017579dbf
Lithiksena <71975651+Lithiksena@users.noreply.github.com>
2020-12-05 11:40:24 +0100

    program 1 :

---
+++ add two numbers and print it through if the sum of those two numbers equal to 10.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int a;
+	int b;
+    cin>>a>>b; 
+    int c=a+b;
+    
+	if (c==10)
+	{
+		cout<<"true";
+	}
+	else
+	{
+		cout<<"false";
+	}
+	return 0;
+}
+
+
+
+
+
+
+/* add two numbers and print it through if the sum of those two numbers equal to 10
+take input of 2 numbers ,a and b
+add a and b 
+check if a and b are equal to 10 
+if true print right number 
+else print wrong number
+*/
+
