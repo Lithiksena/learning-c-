b18acc8fe3f6ac17d5396878b5d16e4f366171c4
Lithiksena <71975651+Lithiksena@users.noreply.github.com>
2021-05-02 16:10:45 +0200

    Add files via upload

---
+++ arrays/reversingarray.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverse()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	int m=0;
+	int q=n-1;
+	 n=q;
+	int temp;
+	while(m<q)
+	{
+		temp=arr[q];
+		arr[q]=arr[m];
+		arr[m]=temp;
+		m++;
+		q--;
+	}
+	for(int i=0;i<=n;i++)
+	{
+		cout<<arr[i]<<"  ";;
+	}
+}
+int main()
+{
+	
+	reverse();
+}
