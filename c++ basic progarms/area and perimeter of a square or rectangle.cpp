c6fb8db0e00ef78a6b9edd145d432f0d47765d79
Lithiksena <71975651+Lithiksena@users.noreply.github.com>
2021-05-02 16:02:00 +0200

    Add files via upload

---
+++ c++ basic progarms/area and perimeter of a square or rectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int length;
+	cin>>length;
+	cout<<"the area of the area would be: "<<length*length;
+	cout<<"the perimeter of this square would would be: "<<length*4;
+	return 0;
+}
+
+
